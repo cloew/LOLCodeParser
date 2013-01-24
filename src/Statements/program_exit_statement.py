2d5cd457f27acd4b35d29e6e3a23d8da34f9fe1c
Chris Loew <cloew123@gmail.com>
2013-01-23 19:29:28 -0700

    Added Program Exit Statement

---
+++ src/Statements/program_exit_statement.py
@@ -0,0 +1,14 @@
+
+class ProgramExitStatement:
+    """ Represents the Program Exit Statement """
+    
+    def __init__(self):
+        """  """
+        
+    def isValidStatement(self, statementString):
+        """ Returns if the string is a valid statement """
+        # May want this to throw an exception if the statement looks almost proper, but fails for some reason
+        return statementString == "KTHXBYE!"
+        
+    def toCCode(self):
+        """ Translates the statement to C Code """
