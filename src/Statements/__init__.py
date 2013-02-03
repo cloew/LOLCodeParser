76f90ac8d47bf786d8e7bfc966a2eb83d0a27e88
Chris Loew <cloew123@gmail.com>
2013-02-03 13:03:27 -0700

    Removed some unnecessary debug text

--- src/Statements/__init__.py
+++ src/Statements/__init__.py
@@ -5,10 +5,10 @@ statementsDirectory = os.path.dirname(__file__)
 
 for modulename in os.listdir(statementsDirectory):
     if modulename.endswith(".py") and modulename != "__init__.py":
-        print modulename
+        #print modulename
         try:
             module = __import__(statementsDirectory, fromlist=[modulename[:-3]])
-            module = getattr(module, modulename[:-3])
+            #module = getattr(module, modulename[:-3])
         except ImportError as error:
             print "Couldn't import", modulename
             print error
